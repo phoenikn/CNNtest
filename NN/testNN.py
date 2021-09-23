1e4d28edb1e8b2b6ed370603b1ba3f0bb6386736
phoenikn <xushijieuser@gmail.com>
2021-09-23 15:55:15 +1000

    test commit

--- NN/testNN.py
+++ NN/testNN.py
@@ -8,6 +8,7 @@ import torch.utils.data as Data
 import numpy as np
 import torchvision.transforms as transforms
 
+#git test
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
